22e0bcaddf56bd1428db676731c7f0c5b2f7d976
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-10-05 08:19:23 +0530

    Wednesday

---
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    string _baseTokenURI;
+    uint256 public _price = 0.01 ether;
+    bool public _paused;
+    uint256 public maxTokenIds = 20;
+    uint256 public tokenIds;
+    IWhitelist whitelist;
+    bool public presaleStarted;
+    uint256 public presaleEnded;
+
+    modifier onlyWhenNotPaused() {
+        require(!_paused, "Contract currently paused");
+        _;
+    }
+}
